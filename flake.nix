a309f39a0306e4f72617397c37bad1bc19b86b7f
Pawel Lisewski <dev@palekiwi.com>
2025-09-21 13:54:42 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -7,6 +7,11 @@
         path = ./templates/rust/fenix;
         description = "Rust project with fenix for toolchain management";
       };
+
+      rust-mcp = {
+        path = ./templates/rust/mcp;
+        description = "Rust starter for MCP server development";
+      };
       
       default = self.templates.rust-fenix;
     };
